19788572ef8e2604b338c90c7ca7b61533e3f1f5
Youssef <sa.youssef@fsr.ac.ma>
2023-07-14 11:16:44 +0100

    Initial commit

---
+++ src/main/java/org/grosserie/msscgrosseriebom/MsscGrosserieBomApplication.java
@@ -0,0 +1,13 @@
+package org.grosserie.msscgrosseriebom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscGrosserieBomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscGrosserieBomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/grosserie/msscgrosseriebom/MsscGrosserieBomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grosserie.msscgrosseriebom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscGrosserieBomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
